2c2daf435dfa82d7783afb88a8bc53da83d6e7db
Hri-r <109955327+Hri-r@users.noreply.github.com>
2024-07-08 14:31:36 +0530

    Update screenread.py

--- PPO_woodcutter/screenread.py
+++ PPO_woodcutter/screenread.py
@@ -2,8 +2,8 @@ import pygetwindow as gw
 from PIL import ImageGrab
 import cv2
 import time
+from skimage.metrics import structural_similarity as ssim
 
-import tensorflow as tf
 import numpy as np
 
 score_value=0
@@ -36,35 +36,28 @@ def getss(program_name):
         print(f"No window found with the title '{program_name}'.")
         return None
 
-def getscore(program_name):
+def getscoreimg(program_name):
     try:
         # Get the window with the specified program name
         window = gw.getWindowsWithTitle(program_name)[0]
 
+
         # Get the coordinates of the window
         left, top, right, bottom = window.left, window.top, window.right, window.bottom
-
-        # Capture the contents of the window using ImageGrab
-        screenshot = ImageGrab.grab(bbox=(left, top, right, bottom))
 
         sc_left, sc_top, sc_right, sc_bottom = left+260, top + 170, right - 300, bottom - 812
         score = ImageGrab.grab(bbox=(sc_left, sc_top, sc_right, sc_bottom))
 
-        # Save or process the screenshot as needed
-        screenshot.save("screenshot.png")
         score.save("score.png")
-        # print("Screenshot captured successfully.")
-        
         img = cv2.imread("score.png", cv2.IMREAD_GRAYSCALE)
         
-        recogmodel  = tf.keras.models.load_model("digit_recognition_model.h5")
-        score_value = np.argmax(recogmodel(np.array([img/255])))
-        # print(score_value)
-        return score_value
-        # cv2.imshow("capture", scoreimg)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
+        return img
 
     except IndexError:
         print(f"No window found with the title '{program_name}'.")
         return None
+    
+def images_are_different(img1, img2, threshold=0.95):
+    # return ImageChops.difference(img1, img2).getbbox() is not None
+    similarity, _ = ssim(img1, img2, full=True)
+    return similarity < threshold
